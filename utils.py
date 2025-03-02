da539541c6f57572b4bc19dfb0dc4cfc7cd96bf7
SirSaliver <126960740+SirSaliver@users.noreply.github.com>
2025-03-02 23:01:30 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -144,7 +144,8 @@ def create_mindmap_figure(graph_data):
             margin=dict(b=0, l=0, r=0, t=0),
             xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
             yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-            plot_bgcolor="black"
+            plot_bgcolor="black",
+            paper_bgcolor="black"
         )
     )
     
